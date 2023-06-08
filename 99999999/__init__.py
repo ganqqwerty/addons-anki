e778619d0ade3e81cd5e7cc9b8ecfd41aef73895
Yuri Katkov <katkov.juriy@gmail.com>
2023-06-09 01:29:50 +0200

    multiple note works, single - no

--- 99999999/__init__.py
+++ 99999999/__init__.py
@@ -1,88 +1,76 @@
 import os
 import sys
+import json
 import re
+from typing import Dict
+from PyQt5.QtWidgets import QAction
 from aqt import mw
-from aqt.utils import showInfo, showWarning
-from aqt.qt import QAction
+from aqt.utils import showWarning
 from anki.hooks import addHook
 
-# Add vendor directory to sys.path
 addon_dir = os.path.dirname(os.path.realpath(__file__))
 vendor_dir = os.path.join(addon_dir, "vendor")
 sys.path.append(vendor_dir)
 import openai
 
-class ChatGPTAddon:
 
-    def __init__(self, note, config, editor):
+class ChatGPTAddon:
+    def __init__(self, note, config):
         self.note = note
         self.config = config
-        self.editor = editor
-        # Setting the API key for openai
         openai.api_key = self.config['apiKey']
 
-    def request_data(self, prompt):
-        # Call to the GPT-3 model and return the response.
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=60
-        )
-        return response
+    def generate(self, browser):
+        prompt = self.create_prompt(self.note)
+        response = self.send_prompt_to_openai(prompt)
+        self.set_response_to_note(response, browser)
 
-    def set_response_to_note(self, response):
-        # Assuming 'text' contains the output text you want to set for the field.
-        # Get the text from the response.
-        text = response.choices[0].text.strip()
+    def create_prompt(self, note):
+        prompt_template = self.config['prompt']
+        pattern = re.compile(r'\{\{\{(\w+)\}\}\}')
+        field_names = pattern.findall(prompt_template)
+        for field_name in field_names:
+            if field_name not in note:
+                raise ValueError(f"Field '{field_name}' not found in note.")
+            prompt_template = prompt_template.replace(f'{{{field_name}}}', note[field_name])
+        return prompt_template
+
+    def send_prompt_to_openai(self, prompt):
+        if self.config.get('emulate') == 'yes':
+            return "This is a fake response for emulation mode."
+
+        try:
+            response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
+            return response.choices[0].text.strip()
+        except Exception as e:
+            showWarning(f"An error occurred while processing the note: {str(e)}")
+            return None
+
+    def set_response_to_note(self, response, browser):
+        if response is None:
+            return
 
         target_field = self.config['targetField']
         if target_field in self.note:
-            self.note[target_field] = text
+            self.note[target_field] = response
         else:
             raise ValueError(f"Target field '{target_field}' not found in note.")
-        self.editor.loadNoteKeepingFocus()
-
-    def generate(self):
-        try:
-            note = self.note
-
-            # Get the prompt from config and replace placeholders with actual note field values.
-            prompt = self.create_prompt(self.config['prompt'], note)
-
-            # Request data from API.
-            response = self.request_data(prompt)
-
-            # Set the response to the note.
-            self.set_response_to_note(response)
-
-        except ValueError as ve:
-            # handle the error, for example, log it or display it to the user
-            showWarning(f"Error: {ve}")
-
-    def create_prompt(self, prompt, note):
-        # Regex pattern for fields enclosed in triple curly brackets.
-        pattern = re.compile(r"{{{(.*?)}}}")
-
-        # Find all field placeholders in the prompt.
-        fields_in_prompt = pattern.findall(prompt)
-
-        # For each placeholder, replace it with the corresponding value from the note.
-        for field in fields_in_prompt:
-            if field in note:
-                prompt = prompt.replace(f"{{{field}}}", note[field])
-            else:
-                raise ValueError(f"Field '{field}' not found in note.")
-
-        return prompt
+        if browser.editor and browser.editor.note and browser.editor.note.id == self.note.id:
+            browser.editor.loadNoteKeepingFocus()
+        else:
+            self.note.flush()
 
 def process_notes(browser):
-    if not browser.editor:
-        raise Exception("No active editor found.")
+    selected_notes = browser.selectedNotes()
+    if not selected_notes:
+        showWarning("No notes selected.")
+        return
 
-    note = browser.editor.note
-    config = mw.addonManager.getConfig(__name__)  # Get the config from the json file
-    ChatGPTAddon(note, config, browser.editor).generate()
+    config = mw.addonManager.getConfig(__name__)
+    for nid in selected_notes:
+        note = mw.col.getNote(nid)
+        chatGPTAddon = ChatGPTAddon(note, config)
+        chatGPTAddon.generate(browser)
 
 def add_menu_option(browser):
     a = QAction("Process Notes with ChatGPT", browser)
@@ -90,5 +78,4 @@ def add_menu_option(browser):
     browser.form.menuEdit.addSeparator()
     browser.form.menuEdit.addAction(a)
 
-# Register the hook function to be called when setting up menus in the browser
 addHook("browser.setupMenus", add_menu_option)
