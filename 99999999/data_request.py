1074012486dace3b2df67a3041f6554ea6f79d41
Yuri Katkov <katkov.juriy@gmail.com>
2023-06-09 18:55:26 +0200

    requests are cleaned up

--- 99999999/data_request.py
+++ 99999999/data_request.py
@@ -4,15 +4,16 @@ import sys
 from aqt.utils import showWarning
 from aqt import mw
 
-
 addon_dir = os.path.dirname(os.path.realpath(__file__))
 vendor_dir = os.path.join(addon_dir, "vendor")
 sys.path.append(vendor_dir)
 import openai
 
-
 config = mw.addonManager.getConfig(__name__)
 openai.api_key = config['apiKey']
+
+from html import unescape
+
 
 def create_prompt(note, prompt_config):
     prompt_template = prompt_config['prompt']
@@ -21,7 +22,11 @@ def create_prompt(note, prompt_config):
     for field_name in field_names:
         if field_name not in note:
             raise ValueError(f"Field '{field_name}' not found in note.")
-        prompt_template = prompt_template.replace(f'{{{field_name}}}', note[field_name])
+        prompt_template = prompt_template.replace(f'{{{{{{{field_name}}}}}}}', note[field_name])
+    # unescape HTML entities and replace line breaks with spaces
+    prompt_template = unescape(prompt_template).replace('<br>', ' ')
+    # remove HTML tags
+    prompt_template = re.sub('<.*?>', '', prompt_template)
     return prompt_template
 
 
@@ -31,7 +36,7 @@ def send_prompt_to_openai(prompt):
 
     try:
         print("Request to chatgpt: ", prompt)
-        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=2000)
         return response.choices[0].text.strip()
     except Exception as e:
         showWarning(f"An error occurred while processing the note: {str(e)}")
