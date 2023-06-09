4c628716675f91790ac21fd5b33eec7c8f3bea53
Yuri Katkov <katkov.juriy@gmail.com>
2023-06-09 03:27:09 +0200

    refactor

--- 99999999/__init__.py
+++ 99999999/__init__.py
@@ -15,18 +15,22 @@ import openai
 
 
 class ChatGPTAddon:
-    def __init__(self, note, config):
-        self.note = note
+    def __init__(self, config):
         self.config = config
         openai.api_key = self.config['apiKey']
 
-    def generate(self, browser):
-        prompt = self.create_prompt(self.note)
+    def generate_for_single_note(self, browser):
+        """Generate text for a single note (editor note)."""
+        prompt = self.create_prompt(browser.editor.note)
         response = self.send_prompt_to_openai(prompt)
-        if browser.editor and browser.editor.note:
-            self.set_response_to_note_in_editor(response, browser.editor)
-        else:
-            self.set_response_to_note(response)
+        self.set_response_to_editor_note(response, browser)
+
+    def generate_for_multiple_notes(self, nid):
+        """Generate text for multiple notes."""
+        note = mw.col.getNote(nid)
+        prompt = self.create_prompt(note)
+        response = self.send_prompt_to_openai(prompt)
+        self.set_response_to_note(response, note)
 
     def create_prompt(self, note):
         prompt_template = self.config['prompt']
@@ -41,6 +45,7 @@ class ChatGPTAddon:
     def send_prompt_to_openai(self, prompt):
         if self.config.get('emulate') == 'yes':
             return "This is a fake response for emulation mode."
+
         try:
             response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
             return response.choices[0].text.strip()
@@ -48,23 +53,31 @@ class ChatGPTAddon:
             showWarning(f"An error occurred while processing the note: {str(e)}")
             return None
 
-    def set_response_to_note(self, response):
-
+    def set_response_to_editor_note(self, response, browser):
+        """Set response to the editor's note."""
         if response is None:
             return
 
         target_field = self.config['targetField']
-        if target_field in self.note:
-            self.note[target_field] = response
+        if target_field in browser.editor.note:
+            browser.editor.note[target_field] = response
         else:
             raise ValueError(f"Target field '{target_field}' not found in note.")
-        self.note.flush()
 
-    def set_response_to_note_in_editor(self, response, editor):
+        browser.editor.loadNoteKeepingFocus()
+
+    def set_response_to_note(self, response, note):
+        """Set response to the note."""
+        if response is None:
+            return
+
         target_field = self.config['targetField']
-        if target_field in self.note:
-            self.note[target_field] = response
-        editor.loadNoteKeepingFocus()
+        if target_field in note:
+            note[target_field] = response
+        else:
+            raise ValueError(f"Target field '{target_field}' not found in note.")
+
+        note.flush()
 
 def process_notes(browser):
     selected_notes = browser.selectedNotes()
@@ -73,15 +86,16 @@ def process_notes(browser):
         return
 
     config = mw.addonManager.getConfig(__name__)
-    for nid in selected_notes:
-        note = mw.col.getNote(nid)
-        if browser.editor and browser.editor.note:
-            note = browser.editor.note
-        chatGPTAddon = ChatGPTAddon(note, config)
-        chatGPTAddon.generate(browser)
+    chatGPTAddon = ChatGPTAddon(config)
+    if len(selected_notes) == 1 and browser.editor and browser.editor.note:
+        chatGPTAddon.generate_for_single_note(browser)
+    else:
+        for nid in selected_notes:
+            chatGPTAddon.generate_for_multiple_notes(nid)
 
 def add_menu_option(browser):
-    a = QAction("Process Notes with ChatGPT", browser)
+    config = mw.addonManager.getConfig(__name__)
+    a = QAction(config['promptName'], browser)
     a.triggered.connect(lambda: process_notes(browser))
     browser.form.menuEdit.addSeparator()
     browser.form.menuEdit.addAction(a)
