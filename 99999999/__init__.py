1926e7d379ca3fe795c29e5e8e9f59b46208caa9
Yuri Katkov <katkov.juriy@gmail.com>
2023-06-11 01:50:57 +0200

    editor button

--- 99999999/__init__.py
+++ 99999999/__init__.py
@@ -1,12 +1,16 @@
 from PyQt5.QtWidgets import QAction, QMenu, QDialog
+from aqt import mw
+from aqt.qt import *
+from aqt.gui_hooks import editor_did_init_buttons
 from anki.hooks import addHook
+import os
 
 from .settings_editor import SettingsWindow
 from .process_notes import process_notes
+from .run_prompt_dialog import RunPromptDialog
+
 
 ADDON_NAME = 'Anki AI Add-on'
-
-from .run_prompt_dialog import RunPromptDialog
 
 
 def create_run_prompt_dialog(browser, prompt_config):
@@ -16,28 +20,48 @@ def create_run_prompt_dialog(browser, prompt_config):
         process_notes(browser, updated_prompt_config)
 
 
-def add_prompts_as_context_menu_items(browser, menu):
+def add_context_menu_items(browser, menu):
     submenu = QMenu(ADDON_NAME, menu)
     menu.addMenu(submenu)
     config = mw.addonManager.getConfig(__name__)
 
     for prompt_config in config['prompts']:
-        a = QAction(prompt_config["promptName"], browser)
-        a.triggered.connect(
-            lambda _, br=browser, pc=prompt_config:
-            create_run_prompt_dialog(br, pc)
-        )
-        submenu.addAction(a)
+        action = QAction(prompt_config["promptName"], browser)
+        action.triggered.connect(lambda _, br=browser, pc=prompt_config: create_run_prompt_dialog(br, pc))
+        submenu.addAction(action)
 
 
-addHook("browser.onContextMenu", add_prompts_as_context_menu_items)
-
-from aqt import mw
-from aqt.qt import *
-
-
-def openSettings():
+def open_settings():
     window = SettingsWindow(mw)
-    window.exec()
+    window.exec_()
 
-mw.addonManager.setConfigAction(__name__, openSettings)
+
+def on_editor_button(editor):
+    prompts = mw.addonManager.getConfig(__name__).get('prompts', [])
+
+    menu = QMenu(editor.widget)
+    for i, prompt in enumerate(prompts):
+        action = QAction(f'Prompt {i + 1}: {prompt["promptName"]}', menu)
+        action.triggered.connect(lambda _, p=prompt: create_run_prompt_dialog(editor.parentWindow, p))
+        menu.addAction(action)
+
+    menu.exec_(editor.widget.mapToGlobal(QPoint(0, 0)))
+
+
+def on_setup_editor_buttons(buttons, editor):
+    icon_path = os.path.join(os.path.dirname(__file__), "icon.svg")
+    btn = editor.addButton(
+        icon=icon_path,
+        cmd="run_prompt",
+        func=lambda e=editor: on_editor_button(e),
+        tip=ADDON_NAME,
+        keys=None,
+        disables=False
+    )
+    buttons.append(btn)
+    return buttons
+
+
+addHook("browser.onContextMenu", add_context_menu_items)
+mw.addonManager.setConfigAction(__name__, open_settings)
+editor_did_init_buttons.append(on_setup_editor_buttons)
