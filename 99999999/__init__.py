77f2d38c86193302a69e731616618beb68c3f618
Yuri Katkov <katkov.juriy@gmail.com>
2023-06-29 14:53:37 +0200

    workds for editing cards from review

--- 99999999/__init__.py
+++ 99999999/__init__.py
@@ -1,25 +1,48 @@
-from PyQt5.QtWidgets import QAction, QMenu, QDialog
 from aqt import mw
 from aqt.qt import *
 from aqt.gui_hooks import editor_did_init_buttons
+from aqt.editor import EditorMode, Editor
+from aqt.browser import Browser
+from aqt.editcurrent import EditCurrent
+from aqt.addcards import AddCards
 from anki.hooks import addHook
 import os
 
 from .settings_editor import SettingsWindow
-from .process_notes import process_notes
+from .process_notes import process_notes, generate_for_single_note
 from .run_prompt_dialog import RunPromptDialog
 
 
 ADDON_NAME = 'IntelliFiller'
 
 
-def create_run_prompt_dialog(parentWindow, prompt_config):
-    '''parentWindow can be either Browser (when we are viewing the list of cards) or EditCurrent when we click edit
-    button while we are reviewing cards. See EditorMode for details '''
-    dialog = RunPromptDialog(parentWindow, parentWindow.selectedNotes(), prompt_config)
+def create_run_prompt_dialog_from_browser(browser, prompt_config):
+    dialog = RunPromptDialog(browser, browser.selectedNotes(), prompt_config)
     if dialog.exec_() == QDialog.DialogCode.Accepted:
         updated_prompt_config = dialog.get_result()
-        process_notes(parentWindow, updated_prompt_config)
+        process_notes(browser, updated_prompt_config)
+
+def create_run_prompt_dialog_from_editor(editor: Editor, prompt_config):
+    '''parentWindow can be either Browser (when we are viewing the list of cards) or EditCurrent when we click edit
+    button while we are reviewing cards. See EditorMode for details '''
+    if editor.editorMode == EditorMode.BROWSER:
+        browser: Browser = editor.parentWindow
+        dialog = RunPromptDialog(browser, browser.selectedNotes(), prompt_config)
+        if dialog.exec_() == QDialog.DialogCode.Accepted:
+            updated_prompt_config = dialog.get_result()
+            process_notes(browser, updated_prompt_config)
+        return
+    if editor.editorMode == EditorMode.EDIT_CURRENT:
+        editCurrentWindow: EditCurrent = editor.parentWindow
+        dialog = RunPromptDialog(editCurrentWindow, [editor.note.id], prompt_config )
+        if dialog.exec_() == QDialog.DialogCode.Accepted:
+            updated_prompt_config = dialog.get_result()
+            generate_for_single_note(editor, updated_prompt_config)
+        return
+    if editor.editorMode == EditorMode.ADD_CARDS:
+        addCardsWindow: AddCards
+        pass
+
 
 
 def add_context_menu_items(browser, menu):
@@ -29,7 +52,7 @@ def add_context_menu_items(browser, menu):
 
     for prompt_config in config['prompts']:
         action = QAction(prompt_config["promptName"], browser)
-        action.triggered.connect(lambda _, br=browser, pc=prompt_config: create_run_prompt_dialog(br, pc))
+        action.triggered.connect(lambda _, br=browser, pc=prompt_config: create_run_prompt_dialog_from_browser(br, pc))
         submenu.addAction(action)
 
 
@@ -44,7 +67,7 @@ def on_editor_button(editor):
     menu = QMenu(editor.widget)
     for i, prompt in enumerate(prompts):
         action = QAction(f'Prompt {i + 1}: {prompt["promptName"]}', menu)
-        action.triggered.connect(lambda _, p=prompt: create_run_prompt_dialog(editor.parentWindow, p))
+        action.triggered.connect(lambda _, p=prompt: create_run_prompt_dialog_from_editor(editor, p))
         menu.addAction(action)
 
     menu.exec_(editor.widget.mapToGlobal(QPoint(0, 0)))
