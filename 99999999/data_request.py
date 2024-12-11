3b960cc99299a328b6d501b710fe86c8794d1eb2
noah <noahzacsol@gmail.com>
2024-12-12 00:26:53 +1100

    removed vendor

--- 99999999/data_request.py
+++ 99999999/data_request.py
@@ -1,13 +1,7 @@
 import re
-import os
 import sys
 from aqt.utils import showWarning
 from aqt import mw
-
-addon_dir = os.path.dirname(os.path.realpath(__file__))
-vendor_dir = os.path.join(addon_dir, "vendor")
-sys.path.append(vendor_dir)
-
 import openai
 from .anthropic_client import SimpleAnthropicClient
 import time;
